e8b6da5f748d2f234b1054cf41a14d411e00635d
Rudy Corona <rcorona@utexas.edu>
2015-10-14 10:55:45 -0500

    Added first version of ROS node using recognizer and test_pipeline.py subscribing to it.

--- test_pipeline.py
+++ test_pipeline.py
@@ -15,6 +15,7 @@ import StaticDialogPolicy
 import ActionSender
 import pygame
 import threading
+from std_msgs.msg import String
 
 class InputFromKeyboard:
     def __init__(self):
@@ -23,106 +24,28 @@ class InputFromKeyboard:
     def get(self):
         return raw_input()
 
-class InputFromSpeech:
+class InputFromSpeechNode:
     def __init__(self):
-        import ctypes
+        rospy.Subscriber('speech', String, self.callback)
+        self.utterance = ""
+        self.waiting = True
 
-        if True: #TODO Allow path specification is None:
-            self.libHandle = ctypes.CDLL("./src/bwi_common/bwi_dialogue/src/nlu_pipeline/speech/speechRecognizer.so")
-        else:
-            self.libHandle = ctypes.CDLL(path)
+    def callback(self, data):
+        self.utterance = data.data
 
-        #Initialize Sphinx. 
-        self.libHandle.sphinx_init()
-        self.libHandle.initMic()
-
-        self.numbers = {"one": '1', "two": '2', "three": '3', "four": '4', "five": '5',
-                        "six": '6', "seven": '7', "eight": '8', "nine": '9'}
-
-    def __del__(self):
-        self.libHandle.closeMic()
-        self.libHandle.sphinx_close()
-
-    def postProcess(self, words):
-        utterance = ""
-
-        for word in words:
-            if word in self.numbers:
-                utterance += self.numbers[word]
-            else:
-                utterance += ' ' + word + ' '
-
-        return utterance
+        self.waiting = False
 
     def get(self):
-        result =  self.getHypString(self.getNBest(1)[0]).split()
+        #Waits for utterance message to arrive. 
+        while self.waiting:
+            pass
 
-        utterance = self.postProcess(result)
+        print self.utterance
 
-        print utterance
+        self.waiting = True
 
-        return utterance
+        return self.utterance
 
-    def record(self):
-        self.libHandle.sphinx_n_best_m(1); 
-
-    def recordToggle(self):
-        #Initializes pygame window to read spacebar presses. 
-        pygame.init()
-        pygame.display.set_mode((1,1))
-
-        running = True
-
-        print "\nPress [SPACEBAR] to record"
-
-        while running:
-            pygame.event.pump()
-            keys = pygame.key.get_pressed()
-
-            if keys[pygame.K_SPACE]:
-                print "Recording"
-
-                #Starts recording from mic to file. 
-                self.libHandle.startRecord()
-
-                #Loops while user is holding space bar. 
-                while keys[pygame.K_SPACE]:
-                    pygame.event.pump()
-                    keys = pygame.key.get_pressed()
-
-                #Stops recording when they release it. 
-                self.libHandle.stopRecord()
-
-                running = False
-            
-        pygame.quit()
-
-    def getNBest(self, n):
-
-        #Creates threads for recording. 
-        thread1 = threading.Thread(target = self.record)
-        thread2 = threading.Thread(target = self.recordToggle)
-
-        #Starts thread execution. 
-        thread1.start()
-        thread2.start()
-
-        #Waits for recording threads to finish. 
-        thread1.join()
-        thread2.join()
-
-        #Opens up results to begin reading from them. 
-        results = open("results.txt", "r")
-
-        #Makes a list of each hypothesis, containing string, confidence, etc.
-        resultList = [line.strip('\n') for line in results]
-
-        hypotheses = [result.split(":") for result in resultList]
-
-        return hypotheses
-
-    def getHypString(self, hypothesis):
-        return hypothesis[0]
 
 class OutputToStdout:
     def __init__(self):
@@ -171,7 +94,7 @@ while True:
     print "token responses: "+str(token_responses)
 
 print "instantiating DialogAgent"
-u_in = InputFromSpeech()
+u_in = InputFromSpeechNode()
 u_out = OutputToStdout()
 static_policy = StaticDialogPolicy.StaticDialogPolicy()
 A = DialogAgent.DialogAgent(parser, generator, grounder, static_policy, u_in, u_out)
