8cdde862d2e169c9c97cc164190c853cffb7457b
Jesse Thomason <jesse@cs.utexas.edu>
2015-11-03 14:42:14 -0600

    changed package name in ROS files

--- src/run_tests.py
+++ src/run_tests.py
@@ -7,9 +7,9 @@ import time
 tests = [('parser_basics',
           'python tests/parser_basics/main.py tests/parser_basics/ont.txt tests/parser_basics/lex.txt tests/parser_basics/samples.txt'),
         ('grounding_recall',
-          'rosrun bwi_dialog src/tests/grounding_recall/main.py tests/grounding_recall/ont.txt tests/grounding_recall/lex.txt tests/grounding_recall/samples.txt'),
+          'rosrun nlu_pipeline src/tests/grounding_recall/main.py tests/grounding_recall/ont.txt tests/grounding_recall/lex.txt tests/grounding_recall/samples.txt'),
         ('action_grounding',
-          'rosrun bwi_dialog src/tests/action_grounding/main.py tests/action_grounding/ont.txt tests/action_grounding/lex.txt tests/action_grounding/utterance_action_pairs.txt')]
+          'rosrun nlu_pipeline src/tests/action_grounding/main.py tests/action_grounding/ont.txt tests/action_grounding/lex.txt tests/action_grounding/utterance_action_pairs.txt')]
 
 # read command line arguments
 tests_to_output = []
