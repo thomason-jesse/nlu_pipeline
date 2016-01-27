8d9f1599a2b98ac1bee9a8fba13a41b945d49061
Aishwarya Padmakumar <aish@cs.utexas.edu>
2016-01-26 19:41:21 -0600

    Precautionary commit

--- src/HISBeliefState.py
+++ src/HISBeliefState.py
@@ -74,8 +74,8 @@ class HISBeliefState:
                     required_params[param_name] = [utterance.referring_params[param_name]]
                 required_params[param_name] = list(set(required_params[param_name]))
                 
-        #print "required_goal = ", required_goal        # DEBUG
-        #print "required_params = ", required_params    # DEBUG
+        print "required_goal = ", required_goal        # DEBUG
+        print "required_params = ", required_params    # DEBUG
         
         # Iterate over the partitions and split any partition that is a 
         # superset of what is required
@@ -151,21 +151,21 @@ class HISBeliefState:
         #print '----------------------------------------------'
         hypothesis_beliefs = dict()
         
-        #print 'Updation'
+        print 'Updation'
         for partition in self.partitions :
             for utterance in n_best_utterances :
-                #print '---------------------------------'
-                #print str(partition)
-                #print str(utterance)
+                print '---------------------------------'
+                print str(partition)
+                print str(utterance)
                
                 hypothesis = (partition, utterance)
                 obs_prob = utterance.parse_prob # Pr(o'/u)
-                #print 'obs_prob = ', obs_prob
+                print 'obs_prob = ', obs_prob
                 type_prob = self.knowledge.action_type_probs[system_action.action_type][utterance.action_type] # Pr(T(u)/T(m))
-                #print 'type_prob = ', type_prob 
+                print 'type_prob = ', type_prob 
                 param_match_prob = int(utterance.match(partition, system_action)) # Pr(M(u)/p,m)
-                #print 'param_match_prob = ', param_match_prob 
-                #print '---------------------------------'
+                print 'param_match_prob = ', param_match_prob 
+                print '---------------------------------'
                 hypothesis_beliefs[hypothesis] = obs_prob * type_prob * param_match_prob * partition.belief 
                     # b(p',u') = k * Pr(o'/u) * Pr(T(u)/T(m)) * Pr(M(u)/p,m) * b(p)
             hypothesis = (partition, '-OTHER-')
