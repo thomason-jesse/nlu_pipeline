0f3273763c86f93e056526eeca47ddb2dab8b2dc
rcorona <rcorona@utexas.edu>
2016-02-03 13:25:14 -0600

    Added folder structure for pickling parser.

--- src/test_pipeline.py
+++ src/test_pipeline.py
@@ -16,6 +16,7 @@ import ActionSender
 import pygame
 import os
 import threading
+import pickle
 from std_msgs.msg import String
 
 class InputFromKeyboard:
@@ -67,17 +68,20 @@ class OutputWithSpeech:
 
         os.system("espeak '" + s + "'")
 
+#Path for argument files. 
+path = './src/nlu_pipeline/src/speech/data/parser/'
+
 print "calling ROSpy init"
 rospy.init_node('test_NLU_pipeline')
 
 print "reading in Ontology"
-ont = Ontology.Ontology(sys.argv[1])
+ont = Ontology.Ontology(path + 'ont.txt')
 print "predicates: " + str(ont.preds)
 print "types: " + str(ont.types)
 print "entries: " + str(ont.entries)
 
 print "reading in Lexicon"
-lex = Lexicon.Lexicon(ont, sys.argv[2])
+lex = Lexicon.Lexicon(ont, path + 'lex.txt')
 print "surface forms: " + str(lex.surface_forms)
 print "categories: " + str(lex.categories)
 print "semantic forms: " + str(lex.semantic_forms)
@@ -108,7 +112,7 @@ while True:
 generator.flush_seen_nodes()
 
 print "instantiating DialogAgent"
-u_in = InputFromSpeechNode()
+u_in = InputFromKeyboard()
 u_out = OutputWithSpeech()
 static_policy = StaticDialogPolicy.StaticDialogPolicy()
 A = DialogAgent.DialogAgent(parser, generator, grounder, static_policy, u_in, u_out)
@@ -127,9 +131,10 @@ while True:
     print "RESULT: "+str(r)
 
 print "reading in training data"
-D = A.read_in_utterance_action_pairs(sys.argv[3])
+D = A.read_in_utterance_action_pairs(path + 'utterance_action_pairs.txt')
 
-if len(sys.argv) > 4 and sys.argv[4] == "both":
+#if len(sys.argv) > 4 and sys.argv[4] == "both":
+if False:
     print "training parser and generator jointly from actions"
     converged = A.jointly_train_parser_and_generator_from_utterance_action_pairs(
         D, epochs=10, parse_beam=30, generator_beam=10)
@@ -139,6 +144,11 @@ else:
         D, epochs=10, parse_beam=30)
 
 print "theta: "+str(parser.learner.theta)
+
+#Pickles trained parser. 
+parserFile = open(path + 'parser.pickle', 'w')
+pickle.dump(parser, parserFile)
+print "Pickled parser"
 
 while True:
     u_out.say("How can I help?")
