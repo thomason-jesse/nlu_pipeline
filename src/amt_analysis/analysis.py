58a4a3344e4fe7189b3691ee3b3bd03b3723b7c6
Aishwarya Padmakumar <aish@cs.utexas.edu>
2016-05-01 15:23:05 -0500

    Corrections in get_statistical_significancewq

--- src/amt_analysis/analysis.py
+++ src/amt_analysis/analysis.py
@@ -1,4 +1,5 @@
-import sys, shutil, pickle, re, csv, math, itertools 
+import sys, shutil, pickle, re, csv, math, itertools, copy 
+from scipy import stats
 from os import listdir
 from os.path import isfile, join, isdir
 sys.path.append('../')
@@ -225,16 +226,19 @@ def get_statistical_significance() :
     results_file = open(path_to_batch + 'results.csv', 'r')
     results_reader = csv.reader(results_file, delimiter=',')
     results_header = results_reader.next()
+    agent_type_idx = results_header.index('agent_type')
     
     summary_file = open(path_to_batch + 'summary.csv', 'r')
-    summary_reader = csv.reader(results_file, delimiter=',')
+    summary_reader = csv.reader(summary_file, delimiter=',')
     summary_header = summary_reader.next()
+    print 'summary_header = ', summary_header
     
-    cols_to_evaluate = summary_header
+    cols_to_evaluate = copy.deepcopy(summary_header)
     cols_to_evaluate.remove('agent_type')
     cols_to_evaluate.remove('performed_action')
     cols_to_evaluate.remove('Number of dialogues')
-    
+    print 'cols_to_evaluate = ', cols_to_evaluate
+     
     means = dict()
     num_rows = dict()
     variances = dict()
@@ -250,24 +254,27 @@ def get_statistical_significance() :
                 num_rows[agent_type] = int(col_val)
     
     for row in results_reader :
-        agent_type = row[0]
+        agent_type = row[agent_type_idx]
         variances[agent_type] = dict()
         for (col_num, col_val) in enumerate(row) :
             col_name = results_header[col_num]
+            print 'col_name = ', col_name
             if col_name in cols_to_evaluate :
                 if col_name not in variances[agent_type] :
                     variances[agent_type][col_name] = 0.0
                 variances[agent_type][col_name] = variances[agent_type][col_name] + (float(col_val) - means[agent_type][col_name]) ** 2
     
     for agent_type in variances :
-        for col_name in variances[agent] :
+        for col_name in variances[agent_type] :
+            print 'agent_type = ', agent_type, ', col_name = ', col_name
             variances[agent_type][col_name] = variances[agent_type][col_name] / num_rows[agent_type]
     
     agent_types = means.keys()            
     agent_pairs = list()
     for i in range(0, len(agent_types)-1) :
-        agent_pairs = agent_pairs + list(itertools.product(agent_types[i], agent_types[i+1:]))        
-    
+        agent_pairs = agent_pairs + list(itertools.product([agent_types[i]], agent_types[i+1:]))        
+    print 'agent_pairs = ', agent_pairs
+ 
     filename = path_to_batch + 'statistical_significance.csv'
     file_handle = open(filename, 'w')
     writer = csv.writer(file_handle, delimiter=',')
@@ -303,7 +310,7 @@ def get_statistical_significance_corrected() :
     results_header = results_reader.next()
     
     summary_file = open(path_to_batch + 'summary_corrected.csv', 'r')
-    summary_reader = csv.reader(results_file, delimiter=',')
+    summary_reader = csv.reader(summary_file, delimiter=',')
     summary_header = summary_reader.next()
     
     cols_to_evaluate = summary_header
