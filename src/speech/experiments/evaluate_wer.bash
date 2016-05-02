c13674d50b7b667b1ef1ef169f57ab80d00b72f3
Rodolfo Corona Rodriguez <rud721@gmail.com>
2016-05-02 15:16:23 -0500

    Evaluated WER for first experiments. Still need to consolidate results.

--- src/speech/experiments/evaluate_wer.bash
+++ src/speech/experiments/evaluate_wer.bash
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-./word_align.pl --ignore-uttid ../k-folds/4_fold/1/recordings/test/transcript1.txt ../k-folds/4_fold/1/decoding/baseline/1-en-us/results.t
+CURRENT_DIR=$( pwd )
+
+for i in $( ls $CURRENT_DIR/k-folds/4_fold/ ); do
+	FOLD=$CURRENT_DIR/k-folds/4_fold/$i
+
+	#Evaluates WER for first test set results. 
+	for j in $( ls -d $CURRENT_DIR/k-folds/4_fold/$i/decoding/baseline/1* ); do
+
+		echo "Evaluating " $j " ..." 
+		echo ""
+
+		./bin/word_align.pl --ignore-uttid $FOLD/recordings/test/transcript1.txt $j/results.txt &> $j/wer.txt
+	done
+
+	#Evaluates WER for second test set results. 
+	for j in $( ls -d $CURRENT_DIR/k-folds/4_fold/$i/decoding/baseline/2* ); do
+		BASELINE=$FOLD/decoding/baseline/$j
+
+		echo "Evaluating " $j " ..."
+		echo ""
+
+		./bin/word_align.pl --ignore-uttid $FOLD/recordings/test/transcript2.txt $j/results.txt &> $j/wer.txt
+	done
+done
