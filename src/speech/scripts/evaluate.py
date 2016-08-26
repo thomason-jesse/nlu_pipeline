030ea627340c6c3227be58a4353022ce78588604
rcorona <rcorona@utexas.edu>
2016-08-26 16:55:24 -0500

    Added evaluation for semantic forms

--- src/speech/scripts/evaluate.py
+++ src/speech/scripts/evaluate.py
@@ -33,6 +33,18 @@ import sys
 import os
 import subprocess
 
+#Adds nlu_pipeline src folder in order to import modules from it. 
+nlu_pipeline_path = '/home/rcorona/catkin_ws/src/nlu_pipeline/src/'
+sys.path.append(nlu_pipeline_path)
+
+#Nlu pipeline modules.
+try:
+    import CKYParser
+    from utils import *
+except ImportError:
+    print 'ERROR: Unable to load nlu_pipeline_modules! Verify that nlu_pipeline_path is set correctly!'
+    sys.exit()
+
 #Stores the binary executables, some of which are used by this script. 
 bin_path = os.path.abspath('../bin')
 
@@ -92,8 +104,8 @@ def wer(result_file_name, evaluation_file_name):
     subprocess.call(args, stdout=evaluation_file, stderr=evaluation_file)
 
     #Deletes temp files and closes evaluation. 
-#os.remove('transcripts.txt')
-#os.remove('hyp.txt')
+    os.remove('transcripts.txt')
+    os.remove('hyp.txt')
     evaluation_file.close()
 
 def correct_in_top_n(result_file_name, evaluation_file_name, n):
@@ -153,9 +165,72 @@ def correct_in_top_n(result_file_name, evaluation_file_name, n):
 
     evaluation_file.close()
 
+def semantic_form(result_file_name, evaluation_file_name, parser_file):
+    result_file = open(result_file_name, 'r')
+
+    #Gets absolute path for evaluation file name. 
+    evaluation_file_name = os.path.abspath(evaluation_file_name)
+
+    #Reads in data. 
+    data = []
+    line = None
+
+    while not line == '':
+        line = result_file.readline()
+
+        #Pound delimits phrase. 
+        if line.startswith('#'):
+            phrase, semantic_form = line.strip().split('#')[1].split(';')
+
+            #Line immediately after phrase is top scoring hypothesis. 
+            line = result_file.readline().strip()
+            hypothesis = line.split(';')[0]
+
+            data.append([semantic_form, hypothesis, phrase])
+
+    #Loads parser. 
+    parser = load_obj_general(parser_file)
+
+    #Counts for evaluation. 
+    count_correct = 0
+    total_count = 0
+
+    #Evaluates data set. 
+    for truth_hyp_phrase in data:
+        true_semantic_form, hyp, phrase = truth_hyp_phrase
+        true_semantic_form = ':'.join(true_semantic_form.split(':')[1:])
+
+        #Gets semantic node from true semantic form. 
+        true_node = parser.lexicon.read_semantic_form_from_str(true_semantic_form, None, None, [], False)
+   
+        #Parses top hypothesis to get semantic node. 
+        hyp_node = parser.most_likely_cky_parse(hyp).next()[0].node
+
+        #Evaluates equality. 
+        if true_node == hyp_node:
+            count_correct += 1
+
+        total_count += 1
+
+        print '**********************************************'
+        print "Ground truth phrase: " + phrase
+        print "Ground truth node: " + str(true_node) 
+        print "Ground truth semantic form: " + parser.print_parse(true_node)
+        print ''
+
+        print "Hyp str: " + hyp 
+        print "Hyp node: " + str(hyp_node)
+        print "Hyp semantic form: " + parser.print_parse(hyp_node)
+        print  ''
+
+        print "Equal: " + str(true_node == hyp_node)
+        print '***********************************************'
+        print ''
+
 def print_usage():
     print 'WER: ./evaluate.py wer [result_file] [evaluation_file_name]'
     print 'Correct hypothesis in top n: ./evaluate.py top_n [result_file] [evaluation_file_name] [n]'
+    print 'Semantic form evaluation: ./evaluate.py semantic_form [result_file] [evaluation_file_name] [parser]'
 
 if __name__ == '__main__':
     if not len(sys.argv) >= 2:
@@ -172,6 +247,12 @@ if __name__ == '__main__':
             print_usage()
         else:
             correct_in_top_n(sys.argv[2], sys.argv[3], sys.argv[4])
+
+    elif sys.argv[1] == 'semantic_form':
+        if not len(sys.argv) == 5:
+            print_usage()
+        else:
+            semantic_form(sys.argv[2], sys.argv[3], sys.argv[4])
 
     else:
         print_usage()
