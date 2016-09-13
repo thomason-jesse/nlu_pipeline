fc602777f38d1c906dab6a37ac1f640835eb8ace
Rodolfo Corona Rodriguez <rud721@gmail.com>
2016-09-12 22:01:14 -0500

    Added post-processing step for adding types to semantic forms in a file.

--- src/speech/scripts/post_process.py
+++ src/speech/scripts/post_process.py
@@ -72,10 +72,68 @@ def populate_result_file_with_semantic_forms(result_file_name, test_file_name):
 
     #Moves temp file to overwrite result file. 
     os.rename('temp.txt', result_file_name)
-    
+
+def add_type_constraints_to_file(file_name): 
+    old_file = open(file_name, 'r')
+    tmp_file = open('temp.txt', 'w')
+
+    for line in old_file:
+        #Must be a parser training file. 
+        if not line.startswith("M : "):
+            tmp_file.write(line)
+        else:
+            #Gets generic semantic form. 
+            semantic_form = line.split('M : ')[1].strip() 
+            
+            #Gets action and generates typed semantic form as necessary.
+            action = semantic_form.split('(')[0]
+
+            if action == 'walk':
+                first_element = semantic_form.split('(')[1]
+
+                #If not 'the', then must be a location, so keep semantic form intact. 
+                if not first_element == 'the':
+                    new_semantic_form = semantic_form
+                
+                #Otherwise, is predicate, so replace generic e type with l (for location). 
+                else:
+                    new_semantic_form = semantic_form.replace('lambda x:e', 'lambda x:l', 1)
+
+                    #Currently only expects one lambda, so assert that this is in fact true. 
+                    if 'lambda x:e' in new_semantic_form:
+                        print "More than one lambda expression in walk command!: " + semantic_form + '\n'
+                        sys.exit()
+
+            elif action == 'searchroom':
+                new_semantic_form = semantic_form
+
+                #Currently expects no lambda expressions, so assert this. 
+                if 'lambda' in new_semantic_form:
+                    print "Lambda expression found in 'searchroom' command!: " + new_semantic_form + '\n'
+                    sys.exit()
+            elif action == 'bring':
+                if 'lambda' in semantic_form:
+                    #Replace generic 'e' type with item type 'i'. 
+                    new_semantic_form = semantic_form.replace('lambda x:e', 'lambda x:i', 1)
+
+                    #Currently only expects one lambda expression, so assert this. 
+                    if 'lambda x:e' in new_semantic_form:
+                        print "More than one lambda expression found in bring command!: " + semantic_form + '\n'
+                        sys.exit()
+                else:
+                    new_semantic_form = semantic_form
+            else:
+                print "Unaccounted for action!: " + action + '\n'
+                sys.exit()
+            
+            #Writes new semantic form to new file. 
+            tmp_file.write('M : ' + new_semantic_form + '\n')
+
+    tmp_file.close()
 
 def print_usage():
     print 'To populate result file with semantic forms: ./post_process.py populate_semantic_forms [result_file_name] [test_file_name]'
+    print 'To add type constraints to semantic forms in a file: ./post_process.py type_constraints [file_name]'
 
 if __name__ == '__main__':
     if not len(sys.argv) >= 2:
@@ -84,6 +142,12 @@ if __name__ == '__main__':
     elif sys.argv[1] == 'populate_semantic_forms':
         if len(sys.argv) == 4:
             populate_result_file_with_semantic_forms(sys.argv[2], sys.argv[3])
+        else:
+            print_usage()
+
+    elif sys.argv[1] == 'type_constraints':
+        if len(sys.argv) == 3:
+            add_type_constraints_to_file(sys.argv[2])
         else:
             print_usage()
 
